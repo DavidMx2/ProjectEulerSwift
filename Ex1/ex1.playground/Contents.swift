b7500e10c1a280dd1919424332d150864326a524
David <david_m_0@hotmail.com>
2023-10-07 17:08:14 -0300

    First exercise playground

---
+++ Ex1/ex1.playground/Contents.swift
@@ -0,0 +1,20 @@
+import Foundation
+import XCTest
+
+class MyTestCase: XCTestCase {
+    
+    /*If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+     The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000. */
+    func solve(input: Int) -> Int {
+        //Your code here
+        return 0
+    }
+    
+    func testFunction() {
+        XCTAssertEqual(solve(input: 10), 23)
+    }
+}
+
+MyTestCase.defaultTestSuite.run()
+
+print("The solution is \(MyTestCase().solve(input: 1000))")
